e1ef955405f43645bca375ef393752da61c959a8
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2021-09-09 00:39:48 +0100

    First attempt at getting octoprint installed

--- stage3/01-install-packages/01-run.sh
+++ stage3/01-install-packages/01-run.sh
@@ -1,29 +1,11 @@
 #!/bin/bash -e
 ## Build Mixxx
-mkdir -p ${BASE_DIR}/.ccache/
-mkdir -p "${ROOTFS_DIR}/ccache"
-mount --bind ${BASE_DIR}/.ccache  "${ROOTFS_DIR}/ccache"
 on_chroot << EOF
-    git clone --branch 2.3 https://github.com/mixxxdj/mixxx.git /code/
-    cd /code/
-    git rev-parse HEAD > /opt/mixxx.version
-    export CCACHE_DIR=/ccache
-    ccache -M 5G
-    export CCACHE_NOCOMPRESS="true"
-    export CTEST_PARALLEL_LEVEL="$(nproc)"
-    export CMAKE_BUILD_PARALLEL_LEVEL="$(nproc)"
-    export PATH="$HOME/.local/bin:$PATH"
-    export GTEST_COLOR="1"
-    export CTEST_OUTPUT_ON_FAILURE="1"
-    export QT_QPA_PLATFORM="offscreen"
-    mkdir -p build && cd build
-    cmake -DKEYFINDER=ON -DFFMPEG=ON -DMAD=ON -DMODPLUG=ON -DWAVPACK=ON -DBULK=ON \
-        -DCMAKE_INSTALL_PREFIX=/usr/ -S /code -B /code/build
-    cmake --build /code/build --target install
-    ccache -s
-    cpack -G DEB
+    sudo pip3 install --upgrade pip
+    sudo pip3 install docker-compose
 EOF
 
-unmount "${BASE_DIR}/.ccache"
+mkdir -p ${ROOTFS_DIR}/home/pi/octoprint/
+cp -r files/docker-compose.yml "${ROOTFS_DIR}/home/pi/octoprint/docker-compose.yml"
+
 mkdir -p "$DEPLOY_DIR"
-cp ${ROOTFS_DIR}/code/build/*.deb "$DEPLOY_DIR/"
